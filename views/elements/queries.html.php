eec0ae6544d3a53243e71c010d7094c5709b3f37
Tom Maiaroto <tom@shift8creative.com>
2012-01-04 19:36:09 -0800

    adding some copy about the queries with regard to how many and how long they took. more to come for this section soon.

--- views/elements/queries.html.php
+++ views/elements/queries.html.php
@@ -1,2 +1,10 @@
-<h2>Queries [<?=count($queries); ?>]</h2>
+<?php
+$total_queries = count($queries);
+$total_query_time = 0;
+foreach($queries as $query) {
+	$total_query_time += $query['explain']['millis'];
+}
+?>
+<h2>Queries</h2>
+<p>There <?php echo ($total_queries == 1) ? 'is':'are'; ?> <span class="li3-perf-stat-value"><?=$total_queries; ?></span> quer<?php echo ($total_queries == 1) ? 'y':'ies'; ?> which took <span class="li3-perf-stat-value"><?=$total_query_time; ?>ms</span> to execute.</p>
 <?=$this->li3perf->printVars($queries); ?>
